0cb8cfa6f85513a66354634c1e338f522f0c6dcf
yeom0325 <tjsfls95@gmail.com>
2024-10-05 16:26:50 +0900

    feat: set up initialize React project setup
    - set package.json
    - set public folder and src folder for React Rendering
    - set launch.json for debug and run project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+//실질적인 javascript 구동부(적어도 실습에서는)
+import React from 'react';
+
+//클릭하면 onSquareClick 이벤트를 통해 value값을 적어놓고 리턴하는 함수
+function Square({value,onSquareClick}){
+    return(
+        <button className = "square" onClick = {onSquareClick}>
+            {value}
+        </button>
+    );
+}
+
+function MyButton(){
+    return (
+        <button>I'm a button</button>
+    );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+export default App;
