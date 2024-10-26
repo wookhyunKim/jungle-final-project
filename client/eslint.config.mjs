537336e4bae0717483f6c6f57939b150988c0efd
yeom0325 <tjsfls95@gmail.com>
2024-10-26 13:44:30 +0900

    #2 refactor: move project structure under 'client' folder

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -23,9 +23,15 @@ export default [
     },
     rules: {
       'semi': ['error', 'always'], // 세미콜론 강제 사용
-      'quotes': ['error', 'single'], // 싱글 쿼트 사용 강제
-      'no-console': ['warn'], // 콘솔 로그 경고
-      'indent': ['error', 2], // 2칸 들여쓰기
+            'quotes': ['error', 'single'], // 싱글 쿼트 사용 강제
+            //'no-console': ['warn'], // 콘솔 로그 경고
+            'indent': ['error', 4], // 2칸 들여쓰기
+            'no-var': 'error', //var 대신 let/const 사용 강제
+            'arrow-parens': ['error', 'always'], //일관성을 위해 파라미터가 하나일때라도 괄호 사용
+            'object-curly-spacing': ['error', 'always'], //중괄호 내 공백 강제,
+            'no-trailing-spaces': 'error', //불필요한 공백 제거
+            'comma-dangle': ['error', 'always-multiline'], //후행 콤마 사용
+            'curly': ['error', 'all'], //모든 조건문에 중괄호 사용 강제
     },
   },
 ];
--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
