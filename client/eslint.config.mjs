ac77f889fc2c8b800454e53218d350365636fad7
yeom0325 <tjsfls95@gmail.com>
2024-10-26 13:53:48 +0900

    #3 feat:set initial setup for server and add tanstack-query

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -4,25 +4,25 @@ import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 
 export default [
-  {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], // 검사할 파일 지정
-    languageOptions: {
-      globals: globals.browser, // 브라우저 환경의 전역 변수 설정
+    {
+        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], // 검사할 파일 지정
+        languageOptions: {
+            globals: globals.browser, // 브라우저 환경의 전역 변수 설정
+        },
     },
-  },
-  pluginJs.configs.recommended, // 기본 ESLint 권장 설정
-  ...tseslint.configs.recommended, // TypeScript 권장 설정
-  pluginReact.configs.flat.recommended, // React 권장 설정
+    pluginJs.configs.recommended, // 기본 ESLint 권장 설정
+    ...tseslint.configs.recommended, // TypeScript 권장 설정
+    pluginReact.configs.flat.recommended, // React 권장 설정
 
-  // 추가 규칙 적용
-  {
-    settings: {
-      react: {
-        version: 'detect', // 설치된 React 버전 자동 감지
-      },
-    },
-    rules: {
-      'semi': ['error', 'always'], // 세미콜론 강제 사용
+    // 추가 규칙 적용
+    {
+        settings: {
+            react: {
+                version: 'detect', // 설치된 React 버전 자동 감지
+            },
+        },
+        rules: {
+            'semi': ['error', 'always'], // 세미콜론 강제 사용
             'quotes': ['error', 'single'], // 싱글 쿼트 사용 강제
             //'no-console': ['warn'], // 콘솔 로그 경고
             'indent': ['error', 4], // 2칸 들여쓰기
@@ -32,6 +32,6 @@ export default [
             'no-trailing-spaces': 'error', //불필요한 공백 제거
             'comma-dangle': ['error', 'always-multiline'], //후행 콤마 사용
             'curly': ['error', 'all'], //모든 조건문에 중괄호 사용 강제
+        },
     },
-  },
 ];
