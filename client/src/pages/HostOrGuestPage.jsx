81ec7fc3d947d45000112da7f9fed7895e2c905e
yeom0325 <tjsfls95@gmail.com>
2024-10-28 13:19:07 +0900

    [#12]:add generate game code
    - generateGameCode method  randomly generate 6 digits by combining letters and numbers

--- client/src/pages/HostOrGuestPage.jsx
+++ client/src/pages/HostOrGuestPage.jsx
@@ -1,18 +1,31 @@
 
 import HostOrGuest from '../components/HostOrGuest'
-import Host from "../components/Host";
+import Host from "../components/Profile";
 import Guest from '../components/Guest';
+import Profile from "../components/Profile"
 import "../styles/hostOrGuestPage.css"
 
+
+function generateGameCode(length = 6) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let code = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        code += characters[randomIndex];
+    }
+    return code;
+  }
+
 const HostOrGuestPage = () => {
+
     return (
         <HostOrGuest>
             <div className="hostorguest">
                 <div className="host">
-                    <Host/>
+                    <Profile role={"HOST"} btnName={"방만들기"} code = {generateGameCode()}/>
                 </div>
                 <div className="guest">
-                    <Guest/>
+                    <Profile role = {"GUEST"} btnName={"코드 입력"}/>
                 </div>
             </div>
         </HostOrGuest>
